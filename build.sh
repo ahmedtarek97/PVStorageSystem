4612c73e4a6538b7f76c0230d4e87d97e287aa9a
ahmedtarek97 <ahmedtrk97@gmail.com>
2024-10-06 06:07:33 +0300

    added Doxyfile and updated build.sh for doxgen generation

--- build.sh
+++ build.sh
@@ -2,34 +2,55 @@
 
 # Function to display usage information
 usage() {
-  echo "Usage: $0 [--clean]"
+  echo "Usage: $0 [--clean] [--doc]"
   echo "  --clean  Clean the build directory"
+  echo "  --doc  generate doxgen documintation in /doc directory"
   exit 1
 }
 
-# Check for the --clean option
-if [[ $# -gt 1 ]]; then
-  usage
-fi
-
+DOC=0
 CLEAN=0
-if [[ $# -eq 1 ]]; then
-  if [[ "$1" == "--clean" ]]; then
-    CLEAN=1
-  else
-    usage
-  fi
-fi
 
-# Create a build directory if it doesn't exist
+for arg in "$@"; do
+  case $arg in
+    --clean)
+        CLEAN=1
+      ;;
+    --doc)
+        DOC=1
+      ;;
+    *)
+        usage
+        exit 1
+      ;;
+  esac
+done
+
+
+# Create a build and doc directories if doesn't exist
 BUILD_DIR="build"
 mkdir -p "$BUILD_DIR"
 
-# Clean the build directory if requested
+DOC_DIR="doc"
+mkdir -p "$DOC_DIR"
+
+# Clean the build and doc directories if requested
 if [[ $CLEAN -eq 1 ]]; then
-  echo "Cleaning build directory..."
-  rm -rf "$BUILD_DIR"/*
+  echo "Cleaning build and doc directories..."
+  rm -rf "$BUILD_DIR"/* "$DOC_DIR"/*
   echo "Build directory cleaned."
+fi
+
+if [[ $DOC -eq 1 ]]; then
+    # Check if Doxygen is installed
+    if command -v doxygen &> /dev/null; then
+        echo "Doxygen is installed. Generating documentation..."
+        # Generate Doxygen documentation (assuming Doxyfile is in the same directory)
+        doxygen Doxyfile
+        echo "Documentation generated successfully!"
+    else
+        echo "Doxygen is not installed. Please install it before generating documentation."
+    fi
 fi
 
 # Change to the build directory
@@ -40,6 +61,7 @@ cmake ..
 
 # Build the project
 cmake --build .
+
 
 # Run the executable
 ./PVSS
--- include/PhotovoltaicPanel.hpp
+++ include/PhotovoltaicPanel.hpp
@@ -1,28 +1,26 @@
 /**
- * \file PhotovoltaicPanel.hpp
- * \brief This file contains the declaration of the PhotovoltaicPanel class.
+ * @file PhotovoltaicPanel.hpp
+ * @brief Contains the declaration of the PhotovoltaicPanel class.
  */
 
 #ifndef PHOTOVOLTAIC_PANEL_H  
 #define PHOTOVOLTAIC_PANEL_H
 
 /**
- * \brief Represents a photovoltic panel and the values that can be read from it.
+ * @brief Represents a photovoltic panel and the values that can be read from it.
  */
 class PhotovoltaicPanel{
 public:
     /**
-     * \brief Constructor for the PhotovoltaicPanel class.
-     * \param power Amount of power the PV is producing in Watts.
-     * \param voltage Voltage that the PV is producing in Volts.
-     * \param current urrent that the PV is producing in Amp.
+     * @brief Constructor for the PhotovoltaicPanel class.
+     * @param power Amount of power the PV is producing in Watts.
+     * @param voltage Voltage that the PV is producing in Volts.
+     * @param current Current that the PV is producing in Amp.
      */
     PhotovoltaicPanel(const unsigned int & power, const unsigned int & voltage, const unsigned int & current);
 
-    /**
-     * \brief Gets the power the PV is producing in Watts
-     * 
-     * \returns the amount of power the PV is producing in Watts
+    /**     
+     * @returns the amount of power the PV is producing in Watts
      */
     unsigned int getPowerProduced() const;   
       
